9f0b119550e83f362706fab1c2f761f17fd690f1
Eunsong Koh <eunsongkohh@gmail.com>
2023-12-23 16:07:36 -0800

    Changed light and dark mode preferences

--- components/card.jsx
+++ components/card.jsx
@@ -12,7 +12,7 @@ export default async function Card() {
         {words.map((word) => (
           <a
             href="#"
-            className="m-8 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            className="m-8 block max-w-sm p-6 bg-gray-800 border border-gray-700 rounded-lg shadow hover:bg-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
           >
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">{word.word}</div>
